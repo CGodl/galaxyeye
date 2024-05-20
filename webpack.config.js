b2cadfa57be9f1ffabefd6b350a2f23101336a7c
Carl Godlewski <cgodlew@gmail.com>
2024-05-20 15:43:59 -0400

    Webpack loading just html

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,37 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html', // output file
-    }),
-  ],
-  mode: 'development'
+  plugins: [new HtmlWebpackPlugin({
+    template: "src/index.html",
+  })],
+  mode: 'development',
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+       // Images
+       {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+       // Fonts and SVGs
+       {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+       // CSS, PostCSS, and Sass
+       {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+    open: true
+  },
 }
